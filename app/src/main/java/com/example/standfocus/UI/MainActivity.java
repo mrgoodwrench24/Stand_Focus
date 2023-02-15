a678033775a8b5f27ff57baaa806d9877b548fbe
michaeltroywhite24 <michaeltroywhite24@gmail.com>
2023-02-15 14:36:15 -0700

    2.15.1

--- app/src/main/java/com/example/standfocus/UI/MainActivity.java
+++ app/src/main/java/com/example/standfocus/UI/MainActivity.java
@@ -10,18 +10,24 @@ import android.widget.TextView;
 import com.example.standfocus.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final long TIMER_INTERVAL = 500L;
+    private static final long MILLISECONDS_IN_SECOND = 1000L;
+    private static final long SECONDS_IN_MINUTE = 60L;
+    private static final long SECONDS_IN_HOUR = 3600L;
+
     private long startTime = 0L;
     private Handler timerHandler = new Handler();
     private TextView standTextView;
+    private boolean isTimerRunning = false;
     private Runnable timerRunnable = new Runnable() {
         @Override
         public void run() {
             long millis = System.currentTimeMillis() - startTime;
-            int seconds = (int) (millis / 1000);
-            int minutes = seconds / 60;
-            int hours = seconds / 3600;
-            standTextView.setText(String.format("%d:%02d", hours, minutes, seconds));
-            timerHandler.postDelayed(this,500);
+            int seconds = (int) (millis / MILLISECONDS_IN_SECOND);
+            int minutes = seconds / (int) SECONDS_IN_MINUTE;
+            int hours = seconds / (int) SECONDS_IN_HOUR;
+            standTextView.setText(String.format("%d:%02d:%02d", hours, minutes, seconds % SECONDS_IN_MINUTE));
+            timerHandler.postDelayed(this, TIMER_INTERVAL);
         }
     };
 
@@ -30,16 +36,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        standTextView = (TextView) findViewById(R.id.standTimeTextView);
+        standTextView = findViewById(R.id.standTimeTextView);
     }
 
     public void startTimer(View view){
-        startTime = System.currentTimeMillis();
-        timerHandler.postDelayed(timerRunnable, 0);
+        if (!isTimerRunning) {
+            startTime = System.currentTimeMillis();
+            timerHandler.postDelayed(timerRunnable, 0);
+            isTimerRunning = true;
+        }
     }
 
     public void stopTimer(View view){
-        timerHandler.removeCallbacks(timerRunnable);
+        if (isTimerRunning) {
+            timerHandler.removeCallbacks(timerRunnable);
+            isTimerRunning = false;
+        }
     }
-
 }
